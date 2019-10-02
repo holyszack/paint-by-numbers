050b44476b4ba21fc2f8ded90e89a805ddc18e36
Zachary Holyszko <zholyszko@dmdconnects.com>
2019-10-01 21:49:15 -0500

    Updates

---
+++ precache-manifest.af888f866dfbb9b4e636de5204dc0948.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3a3776879967a3fee23ef4cc0ea36e",
+    "url": "/paint-by-numbers/index.html"
+  },
+  {
+    "revision": "ee72da48f6a58f88f0ab",
+    "url": "/paint-by-numbers/static/css/2.1755982d.chunk.css"
+  },
+  {
+    "revision": "2f58ccc2f4c6efa8b705",
+    "url": "/paint-by-numbers/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "ee72da48f6a58f88f0ab",
+    "url": "/paint-by-numbers/static/js/2.d49a365f.chunk.js"
+  },
+  {
+    "revision": "2f58ccc2f4c6efa8b705",
+    "url": "/paint-by-numbers/static/js/main.48769e5c.chunk.js"
+  },
+  {
+    "revision": "79f14ae058131d8e7599",
+    "url": "/paint-by-numbers/static/js/runtime~main.81f6ccef.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/paint-by-numbers/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/paint-by-numbers/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/paint-by-numbers/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/paint-by-numbers/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/paint-by-numbers/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/paint-by-numbers/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/paint-by-numbers/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/paint-by-numbers/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/paint-by-numbers/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/paint-by-numbers/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/paint-by-numbers/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/paint-by-numbers/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/paint-by-numbers/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/paint-by-numbers/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/paint-by-numbers/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/paint-by-numbers/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b5d55b708093bc83758a903c032c5e8.js"
+  "/paint-by-numbers/precache-manifest.af888f866dfbb9b4e636de5204dc0948.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/paint-by-numbers/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
